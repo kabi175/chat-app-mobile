efab4d2d5245450abaf2370693597dad900421f4
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-24 17:30:11 +0530

    add in-memory user db

--- src/data/user.db.ts
+++ src/data/user.db.ts
@@ -1,20 +1,40 @@
-import { IUser } from '../model';
 import { singleton } from 'tsyringe';
+import { IUser } from '../model';
+import { IUserDB } from '../model/user.db';
 
-@singleton()
-export class InMemoryUserDB {
-	user?: IUser;
+/**
+ * In-memory User DataBase implementation.
+ */
+export class InMemoryUserDB implements IUserDB {
 	friends: IUser[];
 	constructor() {
 		this.friends = [];
 	}
-	addFriend(user: IUser) {
+	async addFriend(user: IUser): Promise<{ error?: Error }> {
 		this.friends.push(user);
+		return {};
 	}
-	removeFriend(user: IUser) {
+	async removeFriend(user: IUser): Promise<{ error?: Error }> {
 		this.friends = this.friends.filter((f) => f.id !== user.id);
+		return {};
 	}
-	removeAllFriends() {
+	async removeAllFriend(): Promise<{ error?: Error }> {
 		this.friends = [];
+		return {};
+	}
+	async getFriends(): Promise<{ error?: Error; friends?: IUser[] }> {
+		return { friends: this.friends };
+	}
+	async getFriendByEmail(
+		email: string
+	): Promise<{ error?: Error; friend?: IUser }> {
+		const friend = this.friends.find((f) => f.email === email);
+		return { friend };
+	}
+	async getFriendByID(
+		id: number
+	): Promise<{ error?: Error; friend?: IUser }> {
+		const friend = this.friends.find((f) => f.id === id);
+		return { friend };
 	}
 }
