682c7bbf1e330da485455375959d1ea3915706d8
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-11-24 17:22:18 +0530

    update MessageDB model

--- src/model/message.db.ts
+++ src/model/message.db.ts
@@ -1,8 +1,21 @@
+import { IUser } from '.';
 import { IMessage } from './message';
 
+/**
+ * MessageDB is a class that represents a database of messages.
+ */
 export interface IMessageDB {
-	addNewMessage(message: IMessage): Promise<{ error?: Error }>;
-	removeMessage(message: IMessage): Promise<{ error?: Error }>;
-	clearMessage(message: IMessage): Promise<{ error?: Error }>;
-	clearUserMessage(userID: number): Promise<{ error?: Error }>;
+	addMessage(message: IMessage): Promise<{ error?: Error }>;
+	getMessagesByID(
+		messageID: string
+	): Promise<{ error?: Error; messages?: IMessage[] }>;
+	getMessagesByUserEmail(
+		userEmail: string
+	): Promise<{ error?: Error; messages?: IMessage[] }>;
+	getMessagesByUserID(
+		userID: number
+	): Promise<{ error?: Error; messages?: IMessage[] }>;
+	removeMessageByID(messageID: string): Promise<{ error?: Error }>;
+	removeUserMessage(user: IUser): Promise<{ error?: Error }>;
+	removeAllMessage(): Promise<{ error?: Error }>;
 }
